b2911f0435ee0e580a8a4e9d3278d0cb550bc69e
thkernel <salut.amos@gmail.com>
2024-09-30 16:45:12 +0000

    update

--- app/javascript/packs/arrival_mails.js
+++ app/javascript/packs/arrival_mails.js
@@ -140,7 +140,7 @@ $(document).on('turbolinks:load', function() {
         timeoutDelay: 200,
         maxValuesShown: 20,
         maxValuesMessage: '{number} selected',
-        addableText: 'Press "Enter" to add {value}',
+        addableText: 'Appuyer "Entrée" ou sur le bouton "+" pour ajouter {value}',
       },
       events: {
         // Allow anything
@@ -148,8 +148,35 @@ $(document).on('turbolinks:load', function() {
 
         // or
 
+        // Decide what is allowed to be added
+        addable: function (value) {
+          // return false or null if you do not want to allow value to be submitted
+          if (value === 'bad') { return false }
+
+          // return new
+
+          // Return the value string
+          return value // Optional - value alteration // ex: value.toLowerCase()
+
+          // Optional - Return a valid data object.
+          // See methods/setData for list of valid options
+          return {
+            text: value,
+            value: value.toLowerCase()
+          }
+
+          // Optional - Return a promise with either a string or data object
+          return new Promise((resolve) => {
+            resolve({
+              text: value,
+              value: value,
+            })
+          })
+        }
+
         
       }
+
     })
 
 });
