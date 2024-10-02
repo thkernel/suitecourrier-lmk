26b3f4ee6f4d649099f534d1e3fba4eca281f11c
thkernel <salut.amos@gmail.com>
2024-10-02 00:10:48 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,7 +126,7 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 
-config.action_mailer.default_url_options = { protocol: "http", host: Rails.application.credentials.dig(:email, :production, :host)  }
+config.action_mailer.default_url_options = { protocol: "https", host: Rails.application.credentials.dig(:email, :production, :host)  }
 config.action_mailer.delivery_method = :smtp
 =begin
 config.action_mailer.smtp_settings = {
